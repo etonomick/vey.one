9b6681f4808a6f271c682010fd35390b06252474
Valery Stepanov <etonomick@gmail.com>
2022-08-13 11:52:01 +0000

    Single project route

---
+++ pages/api/projects/[id].js
@@ -0,0 +1,14 @@
+import { supabase } from "../../../utils/supabaseClient";
+import withAuth from "../../../utils/withAuth";
+
+async function handler(req, res) {
+    
+    const { id } = req.query
+
+    const { data, error } = await supabase.from("projects").select("*").eq("user_id", req.user.id).eq("id", id).single()
+    if (error) return res.status(500).json(error)
+    res.status(200).json(data)
+
+}
+
+export default withAuth(handler)
--- pages/dashboard/[id].js
+++ pages/dashboard/[id].js
@@ -5,16 +5,28 @@ import Editor from "../../components/layouts/Editor";
 import Heading from "../../components/ui/Heading";
 import Text from "../../components/ui/Text";
 import Button from "../../components/ui/Button";
+import useSWR from "swr";
+import withToken from "../../utils/withToken"
+import fetcher from "../../utils/fetcher"
 
 export default function Project() {
 
     const router = useRouter()
     const { id } = router.query
 
+    const { data, error } = useSWR(withToken(`/api/projects/${id}`), fetcher)
+
+    if (error) {
+        return (
+            <div>error</div>
+        )
+    }
+
     return (
         <div className="flex flex-col gap-5">
-            <Heading>{id} <Button>Start</Button></Heading>
-            <Text>{id}</Text>
+            <Heading>{data.title}</Heading>
+            <Text>{data.description}</Text>
+            {data && JSON.stringify(data)}
             <Editor />
         </div>
     )
