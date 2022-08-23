7c4bde6bc171832514f1b4543a85fe3fa9b7d9f5
Valery Stepanov <etonomick@gmail.com>
2022-08-23 08:44:27 +0000

    Sign in with Google

--- components/Auth.js
+++ components/Auth.js
@@ -3,21 +3,31 @@ import { supabase } from "../utils/supabaseClient";
 import Button from "./ui/Button";
 import Vey from "./ui/Vey";
 import Heading from "./ui/Heading";
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaGoogle } from "react-icons/fa";
 
 export default function Auth() {
 
     return (
         <div className="w-full min-h-screen flex flex-col items-center justify-center gap-5">
+
             <Vey />
-            <div className="bg-white p-8 rounded-3xl flex flex-col gap-5 items-center justify-center">
-            
-            <Heading>Sign In</Heading>
-            <Button onClick={() => {
-                supabase.auth.signInWithOAuth({
-                    provider: "discord"
-                })
-            }}><FaDiscord /> Discord</Button>
+
+            <div className="bg-neutral-100 p-8 rounded-3xl flex flex-col gap-5 items-center justify-center">
+
+                <Heading>Sign In</Heading>
+
+                <Button onClick={() => {
+                    supabase.auth.signInWithOAuth({
+                        provider: "discord"
+                    })
+                }}><FaDiscord /> Discord</Button>
+
+                <Button onClick={() => {
+                    supabase.auth.signInWithOAuth({
+                        provider: "google"
+                    })
+                }}><FaGoogle /> Google</Button>
+
             </div>
         </div>
     )
